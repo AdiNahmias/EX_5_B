bfcb767836ea41a354d3e6f14b3333dcfbea0734
AdiNahmias <adi.nahmias15@gmail.com>
2023-06-06 17:49:13 +0300

    fix

--- sources/MagicalContainer.cpp
+++ sources/MagicalContainer.cpp
@@ -4,22 +4,76 @@
 
 namespace ariel{
 
+
+bool static isPrime(int number){
+        if (number <= 1) {
+        return false;
+    }
+    for (int i = 2; i * i <= number; ++i) {
+        if (number % i == 0) {
+            return false;  // Number is divisible, hence not prime
+            }
+        }
+        return true;
+}
+
+bool comparePointers(const int* a, const int* b) {
+    return (*a) < (*b);
+}
+
 void MagicalContainer::addElement(int element) {
+    //check if the element alredy in the elments vector
+    for(int run : elements){
+        if(run == element){
+            return;
+        }
+    }
+    if(isPrime(element)){
+        prime_elements.push_back(new int (element));
+    }
     elements.push_back(element);
+    
+    std::sort(elements.begin(), elements.end());
+    std::sort(prime_elements.begin(), prime_elements.end(), comparePointers);
+
 }
 
 void MagicalContainer::removeElement(int element) {
+    bool in_elemnts = false;
+    for(int run : elements){
+        if(run == element){
+            in_elemnts = true;
+        }
+    }
+    if(!in_elemnts){
+        throw std::runtime_error("The Element not in the container!");
+    }
     for (auto i = elements.begin(); i != elements.end(); ++i) {
         if (element == *i) {
             elements.erase(i);
             break;
         }
     }
+    if(isPrime(element)){
+        for (auto i = prime_elements.begin(); i != prime_elements.end(); ++i) {
+            int *find_element = new int(element);
+            if (find_element == *i) {
+                prime_elements.erase(i);
+                delete find_element;
+                break;
+            }
+    }
+
+    }
 }
 
 std::vector<int>& MagicalContainer::getElements(){
     return this->elements;
 }
+
+std::vector<int*>& MagicalContainer::getPrimeContainer(){
+                return this->prime_elements;
+            }
 
 void MagicalContainer::setElements(std::vector<int>& elements){
     this->elements = elements;
--- sources/MagicalContainer.hpp
+++ sources/MagicalContainer.hpp
@@ -4,318 +4,255 @@
 #include <vector>
 #include <algorithm> // for std::sort
 #include <cstddef>
+#include <stdexcept>
+
 
 namespace ariel{
 
 class MagicalContainer {
 
     private:
-    std::vector<int> elements;
+        std::vector<int> elements;
+        std::vector<int*> prime_elements;
 
     public:
-    MagicalContainer() = default; // Default constructor
-    MagicalContainer(const MagicalContainer& other) = default; // Copy constructor
-    ~MagicalContainer() = default; // Destructor
+        MagicalContainer() = default; // Default constructor
+        MagicalContainer(const MagicalContainer& other) = default; // Copy constructor
+        std::vector<int*>& getPrimeContainer();
+        std::vector<int>& getElements();
+        void setElements(std::vector<int>& elements);
+        void addElement(int element);
+        void removeElement(int element);
+        int size() const;
 
-    std::vector<int>& getElements();
-    void setElements(std::vector<int>& elements);
-    void addElement(int element);
-    void removeElement(int element);
-    int size() const;
-
-    //for tidy
-    
-    // Copy assignment operator
-    MagicalContainer& operator=(const MagicalContainer& other) {
-        if (this != &other) {
-            // Perform deep copy of the container
-            elements = other.elements;
+        bool operator==(const MagicalContainer& other) const {
+            return elements == other.elements;
         }
-        return *this;
-    }
-
-    // Move constructor
-    MagicalContainer(MagicalContainer&& other) noexcept = default;
-
-    // Move assignment operator
-    MagicalContainer& operator=(MagicalContainer&& other) noexcept {
-        if (this != &other) {
-            // Move the container from the other object
-            elements = std::move(other.elements);
+        ~MagicalContainer() {
+            for (int* prime : prime_elements) {
+                delete prime;
+            }
         }
-        return *this;
-    }
 
+        //----------------------------------------------------
+        // Copy assignment operator
+        MagicalContainer& operator=(const MagicalContainer& other) {
+            if (this != &other) {
+                // Perform deep copy of the container
+                elements = other.elements;
+            }
+            return *this;
+        }
+        // Move constructor
+        MagicalContainer(MagicalContainer&& other) noexcept = default;
 
-    class AscendingIterator {
-        
+        // Move assignment operator
+        MagicalContainer& operator=(MagicalContainer&& other) noexcept {
+            if (this != &other) {
+                // Move the container from the other object
+                elements = std::move(other.elements);
+            }
+            return *this;
+            
+        }
+
+    class Iterator {
         private:
-
             MagicalContainer& container;
             int index = 0;
+            bool start_index = true;
+            
+        public:
+            Iterator(MagicalContainer& container) : container(container){}
+            Iterator(const Iterator& other) : container(other.container), index(other.index) , start_index(other.start_index) {}
+            virtual ~Iterator() = default;
+            virtual Iterator& begin() = 0;
+            virtual Iterator& end() = 0;
+            virtual int& operator*() = 0;
+            virtual Iterator& operator++() = 0;
 
+            int getIndex() const{
+                return index;
+            }
+
+            void setIndex(int other){
+                index = other;
+            }
+
+            MagicalContainer& getContainerIterator() const{
+                return container;
+            }
+
+            bool get_move_start() const{
+                return start_index;
+            }
+
+            void set_move_start(bool other){
+                start_index = other;
+            }
+
+            bool operator==(const Iterator& other) const{
+                if(typeid(*this) != typeid(other)){
+                    throw std::runtime_error("there is diffrent iterators");
+                }
+                return (this->container == other.container) && (this->index == other.index);
+            }
+
+            bool operator!=(const Iterator& other) const {
+                return !(*this == other);
+            }
+
+            bool operator>(const Iterator& other) const{
+                return index > other.index;
+            }
+
+            bool operator<(const Iterator& other) const{
+                return index < other.index;
+            }
+
+            //----------------------------------------------------
+            // Copy assignment operator
+            Iterator& operator=(const Iterator& other) {
+                if(container.getElements() != other.container.getElements()){
+                    throw std::runtime_error("not the same containar");
+                }
+
+                if (this != &other) {
+                    container = other.container;
+                    index = other.index;
+                    start_index = other.start_index;
+                }
+                return *this;
+            }
+            // Move constructor
+            Iterator(Iterator&& other) noexcept : container(other.container), index(other.index) , start_index(other.start_index) {}
+
+            // Move assignment operator
+            Iterator& operator=(Iterator&& other) noexcept {
+                if (this != &other) {
+                container = std::move(other.container);
+                index = other.index;
+                start_index = other.start_index;
+                }
+                return *this;
+            }
+    };
+
+
+
+    class AscendingIterator : public Iterator {
+ 
         public:
 
-            AscendingIterator(const AscendingIterator& copy_container):container(copy_container.container){}//Copy constructor
-            ~AscendingIterator() = default; // Destructor
-
-            AscendingIterator(MagicalContainer& container): container(container){
-            std::vector<int> sortedElements = container.getElements();
-            std::sort(sortedElements.begin(), sortedElements.end());
-            this->container.setElements(sortedElements);
+            AscendingIterator(MagicalContainer& container): Iterator(container){}
+            int& operator*() override {
+                return this->getContainerIterator().getElements()[static_cast<std::vector<int>::size_type>(this->getIndex())];
             }
 
-            
-            int& operator*() const {
-                return container.getElements()[static_cast<std::vector<int>::size_type>(index)];
-            }
-
-            AscendingIterator& operator++(){
-                this->index = this->index + 1;
+            AscendingIterator& operator++() override{
+                if(this->getIndex() == this->getContainerIterator().getElements().size()){
+                    throw std::runtime_error("Iterator at the end index");
+                }
+                this->setIndex(this->getIndex() + 1);
                 return *this;
             }
 
             bool operator==(const AscendingIterator& other_iterator) const {
-                        return index == other_iterator.index;
+                        return getIndex() == other_iterator.getIndex();
             }
             bool operator!=(const AscendingIterator& other_iterator) const {
-                        return index != other_iterator.index;
+                        return getIndex() != other_iterator.getIndex();
             }
             
-            AscendingIterator begin() const{
-                    AscendingIterator begin_iterator(container);
-                    return begin_iterator;
+            AscendingIterator& begin() override{
+                    this->setIndex(0);
+                    return *this;
             }
-
-            
-            AscendingIterator end() const{
-                AscendingIterator iterator_end(container);
-                iterator_end.index = container.size();
-                return iterator_end;
-            }
-
-            // Copy assignment operator
-            AscendingIterator& operator=(const AscendingIterator& other) {
-                if (this != &other) {
-                    container = other.container;
-                    index = other.index;
-                }
-                return *this;
-            }
-           
-
-            // Move constructor
-            AscendingIterator(AscendingIterator&& other) noexcept : container(other.container), index(other.index) {}
-
-            // Move assignment operator
-            AscendingIterator& operator=(AscendingIterator&& other) noexcept {
-                if (this != &other) {
-                    container = std::move(other.container);
-                    index = other.index;
-                }
+            AscendingIterator& end() override {
+                this->setIndex(static_cast<int>(this->getContainerIterator().getElements().size()));
                 return *this;
             }
 
-            
 
-            
+       
+    };
 
-
-        };
-
-
-
-    class SideCrossIterator {
-
-    private:
-        MagicalContainer& container;
-        int index = 0;
-
+    class SideCrossIterator: public Iterator {
+        
     public:
 
-        SideCrossIterator(MagicalContainer& container):container(container){
-            std::vector<int>& sortedElements = container.getElements();
-            std::sort(sortedElements.begin(), sortedElements.end());
-            size_t start = 0;
-            size_t end = sortedElements.size() -1;
-            std::vector<int> crossElements(static_cast<size_t>(sortedElements.size()), 0);
-            //size_t ind = 0;
-            for (size_t i = 0; i < container.size() ; i+=2){
-                if(start == end){
-                    crossElements[i] = sortedElements[start]; 
-                    break;  
-                }
-                crossElements[i] = sortedElements[start];
-                crossElements[i+1] = sortedElements[end];
-                start++;
-                end--; 
+        SideCrossIterator(MagicalContainer& container):Iterator(container){}
+        int& operator*() override {
+            if (this->get_move_start()) {
+                return this->getContainerIterator().getElements()[static_cast<std::vector<int>::size_type>(this->getIndex())];
             }
+            return this->getContainerIterator().getElements()[this->getContainerIterator().getElements().size() - static_cast<std::vector<int>::size_type>(this->getIndex())];
+        }
 
-            this->container.setElements(crossElements);
+        SideCrossIterator& operator++() override {
+        auto containerSize = static_cast<int>(this->getContainerIterator().getElements().size());
+        if (this->getIndex() == containerSize) {
+            throw std::runtime_error("Iterator at the end index");
+        }
+
+        if (this->get_move_start()) { // Iterator on the left side
+            set_move_start(false); // Iterator goes to the right side
+            if (this->getIndex() == containerSize / 2) { // If the index is at the end
+                this->setIndex(containerSize);
+            } else {
+                this->setIndex(this->getIndex() + 1);
+            }
+        } else {
+            this->set_move_start(true); // Iterator goes to the left side
+        }
+
+        return *this;
+    }
+
+
+         SideCrossIterator& begin() override{
+            this->setIndex(0);
+            this->set_move_start(true);
+            return *this;
 
         }
 
-        //copy constructor
-        SideCrossIterator(const SideCrossIterator& other_container) : container(other_container.container){} //Copy constructor
-        ~SideCrossIterator() = default; //Destructor
-
-        int& operator*() const {
-            return container.getElements()[static_cast<std::vector<int>::size_type>(index)];
-        }
-        SideCrossIterator& operator++(){
-            // Increment the index
-            index++;
+        SideCrossIterator& end() override{
+            this->setIndex(static_cast<int>(this->getContainerIterator().getElements().size()));
+            this->set_move_start(false);
             return *this;
         }
-       
-        bool operator==(const SideCrossIterator& other) const{
-            return index == other.index;
-        }
-
-        bool operator!=(const SideCrossIterator& other) const{
-            return index != other.index;
-        }
-
-        bool operator>(const SideCrossIterator& other) const{
-            return index > other.index;
-        }
-
-        bool operator<(const SideCrossIterator& other) const{
-            return index < other.index;
-        }
-
-        SideCrossIterator begin() const{
-            return SideCrossIterator(container);
-        }
-
-        // Return an iterator that point to the end of the container (one past the last element)
-        SideCrossIterator end() const{
-            SideCrossIterator iterator_end(container);
-            iterator_end.index = container.size();
-            return iterator_end;
-        }
-
-        // Copy assignment operator
-        SideCrossIterator& operator=(const SideCrossIterator& other) {
-            if (this != &other) {
-                container = other.container;
-                index = other.index;
-            }
-            return *this;
-        }
-
-        SideCrossIterator(SideCrossIterator&& other) noexcept
-            : container(other.container), index(other.index) {
-            // Move constructor implementation
-        }
-
-        SideCrossIterator& operator=(SideCrossIterator&& other) noexcept {
-            if (this != &other) {
-                container = std::move(other.container);
-                index = other.index;
-            }
-            return *this;
-        }
-
 
     };
 
-    class PrimeIterator  {
-
-        private:
-        
-            MagicalContainer& container;
-            int index = 0;
-
-            //help function
-            bool static isPrime(int number){
-                if (number <= 1) {
-                return false;
-            }
-            for (int i = 2; i * i <= number; ++i) {
-                if (number % i == 0) {
-                    return false;  // Number is divisible, hence not prime
-                    }
-                }
-                return true;
-            }
+    class PrimeIterator: public Iterator{
 
         public:
-            
-            PrimeIterator(const PrimeIterator& other_container) : container(other_container.container){} //Copy constructor
-            ~PrimeIterator() = default; //Destructor
 
-            PrimeIterator(MagicalContainer& container):container(container){
-                std::vector<int> primeElements;
-                for (size_t i = 0; i < container.getElements().size(); i++)
-                {
-                    if (isPrime(container.getElements()[i])) {
-                        primeElements.push_back(container.getElements()[i]);
-                    }
-                }
-                container.setElements(primeElements);
+            PrimeIterator(MagicalContainer& container):Iterator(container){}
+
+            int& operator*() override {
+            return *(this->getContainerIterator().getPrimeContainer()[static_cast<std::vector<int>::size_type>(this->getIndex())]);
             }
 
-            int& operator*() const {
-                return container.getElements()[static_cast<std::vector<int>::size_type>(index)];
+            PrimeIterator& operator++() override{
+                if (this->getIndex() == this->getContainerIterator().getPrimeContainer().size()) {
+                throw std::runtime_error("Iterator at the end index");
             }
-             PrimeIterator& operator++(){
-                // Increment the index
-                index++;
+                this->setIndex(this->getIndex() + 1);
                 return *this;
             }
-            
-            bool operator==(const PrimeIterator& other) const{
-                return index == other.index;
-            }
 
-            bool operator!=(const PrimeIterator& other) const{
-                return index != other.index;
-            }
-
-            bool operator>(const PrimeIterator& other) const{
-                return index > other.index;
-            }
-
-            bool operator<(const PrimeIterator& other) const{
-                return index < other.index;
-            }
-
-            PrimeIterator begin() const{
-                return PrimeIterator(container);
-            }
+            PrimeIterator& begin() override{
+                        this->setIndex(0);
+                        return *this;
+                    }
 
             // Return an iterator that point to the end of the container (one past the last element)
-            PrimeIterator end() const{
-                PrimeIterator iterator_end(container);
-                iterator_end.index = container.size();
-                return iterator_end;
-            }
-
-
-            PrimeIterator& operator=(const PrimeIterator& other) {
-                if (this != &other) {
-                    container = other.container;
-                    index = other.index;
-                }
+            PrimeIterator& end() override{
+                this->setIndex(static_cast<int>(this->getContainerIterator().getPrimeContainer().size()));
                 return *this;
             }
-
-            PrimeIterator(PrimeIterator&& other) noexcept
-                : container(other.container), index(other.index) {
-                // Move constructor implementation
-            }
-
-            PrimeIterator& operator=(PrimeIterator&& other) noexcept {
-                if (this != &other) {
-                    container = std::move(other.container);
-                    index = other.index;
-                }
-                return *this;
-            }
-            
-
-
 
         };
     };
